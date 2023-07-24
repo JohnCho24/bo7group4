d26c649277e5b2ff4d6ba0fbb54e7d4db3c0a816
JohnCho24 <johnleecho@hotmail.com>
2023-07-24 18:09:53 -0400

    reset homepage

--- app/src/main/java/com/b07group4/HomePage.java
+++ app/src/main/java/com/b07group4/HomePage.java
@@ -2,51 +2,16 @@ package com.b07group4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class HomePage extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-        if(user == null) {
-            Intent intent = new Intent(getApplicationContext(), LoginShopper.class);
-            startActivity(intent);
-            finish();
-        }
-        else {
-            textView.setText(user.getEmail());
-        }
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginShopper.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 
     public void clickOwner(View view){
