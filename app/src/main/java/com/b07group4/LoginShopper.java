ad08de0bd809dab5460941e0b7f34a9efce56462
JohnCho24 <johnleecho@hotmail.com>
2023-07-24 18:08:41 -0400

    changes to loginshopper

--- app/src/main/java/com/b07group4/LoginShopper.java
+++ app/src/main/java/com/b07group4/LoginShopper.java
@@ -1,104 +1,92 @@
 package com.b07group4;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginShopper extends AppCompatActivity {
 
-    TextInputEditText editTextUsernameShopper, editTextPasswordShopper;
-    Button buttonLogin;
-    FirebaseAuth mAuth;
-    ProgressBar progressBar;
-    TextView textView;
+    FirebaseDatabase db;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), HomePage.class);
-            startActivity(intent);
-            finish();
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_shopper);
-
-        mAuth = FirebaseAuth.getInstance();
-        editTextUsernameShopper = findViewById(R.id.UsernameShopper);
-        editTextPasswordShopper = findViewById(R.id.PasswordShopper);
-        buttonLogin = findViewById(R.id.LoginShopperButton);
-        progressBar = findViewById(R.id.progressBar);
-        textView = findViewById((R.id.registerNow));
-        textView.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegisterShopper.class);
-                startActivity(intent);
-                finish();
-            }
-        }));
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progressBar.setVisibility((View.VISIBLE));
-                String username, password;
-                username = String.valueOf(editTextUsernameShopper.getText());
-                password = String.valueOf(editTextPasswordShopper.getText());
-
-                if(TextUtils.isEmpty(username)) {
-                    Toast.makeText(LoginShopper.this, "Enter username", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(password)) {
-                    Toast.makeText(LoginShopper.this, "Enter password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                mAuth.signInWithEmailAndPassword(username, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), HomePage.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    Toast.makeText(LoginShopper.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
+        setContentView(R.layout.activity_login_owner);
+        db = FirebaseDatabase.getInstance();
     }
 
     public void clickHome(View view){
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
+    }
+
+    public void clickSignup(View view){
+        Intent intent = new Intent(this, RegisterOwner.class);
+        startActivity(intent);
+    }
+
+    public void onClickLogin(View view){
+
+        // Username
+        EditText userText = (EditText) findViewById(R.id.ownerUsername);
+        String username = userText.getText().toString();
+
+        // Password
+        EditText userPass = (EditText) findViewById(R.id.ownerPassword);
+        String password = userPass.getText().toString();
+
+        // Blank
+        if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter username & password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        DatabaseReference usersRef = db.getReference("Owners");
+        usersRef.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // User exists
+                if (dataSnapshot.exists()) {
+
+                    // Check if password is correct
+                    if(dataSnapshot.child("password").getValue(String.class).equals(password)){
+                        Toast.makeText(LoginOwner.this, "Login Successful ", Toast.LENGTH_SHORT).show();
+
+                        // Blank
+                        userText.setText("");
+                        userPass.setText("");
+
+                        //
+
+                    }
+                    else{
+                        Toast.makeText(LoginOwner.this, "Password Incorrect", Toast.LENGTH_SHORT).show();
+                        userPass.setText("");
+                    }
+                }
+
+                // User not found
+                else {
+                    Toast.makeText(LoginOwner.this, "User not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
 }
