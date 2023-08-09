040658edc0087bee190435d19e15adfea8007fa7
smarthkaul <105581386+smarthkaul@users.noreply.github.com>
2023-08-09 19:30:48 -0400

    wrote tests for homepresenter (which hopefully work))
    now gonna work on ownerlogin tests

--- app/src/test/java/com/b07group4/HomePresenterTest.java
+++ app/src/test/java/com/b07group4/HomePresenterTest.java
@@ -1,16 +1,33 @@
 package com.b07group4;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+import com.b07group4.auth.AuthContract;
+import com.b07group4.auth.home.HomePresenter;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 
 public class HomePresenterTest {
+    @Mock
+    AuthContract.Home.View view;
+    @Mock
+    HomePresenter presenter;
 
     @Test
     public void onClickAsOwner() {
+        presenter.onClickAsOwner();
+        verify(view).goToOwnerAuth();
     }
 
     @Test
     public void onClickAsShopper() {
+        presenter.onClickAsShopper();
+        verify(view).goToShopperAuth();
     }
 }
--- app/src/test/java/com/b07group4/OwnerLoginPresenterTest.java
+++ app/src/test/java/com/b07group4/OwnerLoginPresenterTest.java
@@ -3,8 +3,13 @@ package com.b07group4;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 
 public class OwnerLoginPresenterTest {
+
 
     @Test
     public void onClickLogin() {
