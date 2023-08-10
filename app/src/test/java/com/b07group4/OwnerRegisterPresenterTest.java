c34b264f35297219363e59d11d27d82417a09f4e
smarthkaul <105581386+smarthkaul@users.noreply.github.com>
2023-08-09 21:45:04 -0400

    mockito!!!!!!!!!!

--- app/src/test/java/com/b07group4/OwnerRegisterPresenterTest.java
+++ app/src/test/java/com/b07group4/OwnerRegisterPresenterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
--- app/src/test/java/com/b07group4/ShopperRegisterPresenterTest.java
+++ app/src/test/java/com/b07group4/ShopperRegisterPresenterTest.java
@@ -3,7 +3,12 @@ package com.b07group4;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ShopperRegisterPresenterTest {
 
     @Test
